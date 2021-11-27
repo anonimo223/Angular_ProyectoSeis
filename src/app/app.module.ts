a0bef2465e910c3760b0e871c47bf45c304edb52
anonimo223 <anguielugo44@gmail.com>
2021-11-27 15:14:42 -0500

    Lugo: Anexo del proyecto Seis de Angular. Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,
+        MatlconModule,
+        MatCardModule,
+        MatButtonModule,
+        MatProgressSpinnerModule } from '@angular/material'; //Error
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
